56ffd67d3cd9c8ddee03d75c40fc0f584c94f652
guywh <guywh@Pavillion>
2022-09-02 18:59:52 -0500

    remove some app.get's in main server file

--- server.js
+++ server.js
@@ -21,39 +21,7 @@ app.use( express.static( path.join(__dirname, '/client/build')) )
 
 if (process.env.NODE_ENV == "production") {
     app.use( express.static( path.join(__dirname, '/client/build')) )
-
-    app.get('/accounts/login', (req, res) => {
-        res.sendFile(indexPath)
-    })
-    
-    app.get('/accounts/register', (req, res) => {
-        res.sendFile(indexPath)
-    })
-    
-    app.get('/accounts/change/password', (req, res) => {
-        res.sendFile(indexPath)
-    })
-
-    app.get('/', (req, res) => {
-        res.sendFile(indexPath)
-    })
 }
-
-app.get('/', (req, res) => {
-    res.sendFile(indexPath)
-})
-
-app.get('/accounts/login', (req, res) => {
-    res.sendFile(indexPath)
-})
-
-app.get('/accounts/register', (req, res) => {
-    res.sendFile(indexPath)
-})
-
-app.get('/accounts/change/password', (req, res) => {
-    res.sendFile(indexPath)
-})
 
 const port = process.env.NODE_EXPRESS_PORT
 
