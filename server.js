3027d6cfb7ac271348db66367d75accdc4213cea
guywh <guywh@Pavillion>
2022-09-02 19:04:31 -0500

    did something

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ require('dotenv').config()
 
 const indexPath = path.join(__dirname, '/client/build/index.html')
 
-app.use( express.static( path.join(__dirname, '/client/build')) )
+// app.use( express.static( path.join(__dirname, '/client/build')) )
 
 if (process.env.NODE_ENV == "production") {
     app.use( express.static( path.join(__dirname, '/client/build')) )
@@ -39,21 +39,21 @@ if (process.env.NODE_ENV == "production") {
     })
 }
 
-app.get('/', (req, res) => {
-    res.sendFile(indexPath)
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(indexPath)
+// })
 
-app.get('/accounts/login', (req, res) => {
-    res.sendFile(indexPath)
-})
+// app.get('/accounts/login', (req, res) => {
+//     res.sendFile(indexPath)
+// })
 
-app.get('/accounts/register', (req, res) => {
-    res.sendFile(indexPath)
-})
+// app.get('/accounts/register', (req, res) => {
+//     res.sendFile(indexPath)
+// })
 
-app.get('/accounts/change/password', (req, res) => {
-    res.sendFile(indexPath)
-})
+// app.get('/accounts/change/password', (req, res) => {
+//     res.sendFile(indexPath)
+// })
 
 const port = process.env.NODE_EXPRESS_PORT
 
