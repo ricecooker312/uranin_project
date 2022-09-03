67a862f290edaa2f08c961beabfb54bede3438ce
guywh <guywh@Pavillion>
2022-09-02 19:12:39 -0500

    changed database connection for production

--- dbconnect.js
+++ dbconnect.js
@@ -2,12 +2,18 @@ const Pool = require('pg').Pool
 
 require('dotenv').config()
 
-const pool = new Pool({
+const devConfig = {
     user: process.env.PG_POOL_POSTGRES_USER,
     host: process.env.PG_POOL_POSTGRES_HOST,
     password: process.env.PG_POOL_POSTGRES_PASSWORD,
     database: process.env.PG_POOL_POSTGRES_DATABASE,
     port: process.env.PG_POOL_POSTGRES_PORT
-})
+}
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL
+}
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig)
 
 module.exports = pool
